65b0d16b10c5abc35c838d1e1fc8596758852eef
kyleziegler-123 <kyleziegler@ufl.edu>
2026-07-29 12:43:53 -0400

    changes

--- calculator.py
+++ calculator.py
@@ -5,6 +5,35 @@ calculator.py
 
 One function per operation, in order.
 """
+#Partner 2 functions
+def add(a:float, b:float):
+    return a + b
+def sub(a:float, b:float):
+    return a - b
+def mul(a:float, b:float):
+    return a * b
+def div(a:float, b:float):
+    try:
+        return b / a
+        raise ZeroDivisionError if a == 0
+    except ZeroDivisionError:
+        print("Error")
+def log(a:float, b:float):
+    try:
+        return math.log(a,b)
+        raise ValueError if b<=0 or b==1 or a<=0
+    except ValueError:
+        print("Error")
+def exp(a:float, b:float):
+    return a**b
+
+git add calculator.py
+#git commit -m "modified calculator p1"
+#git push
+       # configure to merge changes
+git pull
+
+
 # First example
 def add(a, b): a+b
 def subtract(a, b): a-b
