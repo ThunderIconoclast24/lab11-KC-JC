f621923d7a8b6d1a07f51e94fbf31b1d3e9e067b
kyleziegler-123 <kyleziegler@ufl.edu>
2026-07-29 12:49:01 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -38,7 +38,7 @@ git pull
 def add(a, b): a+b
 def subtract(a, b): a-b
 def multiply(a, b): a*b
-def divide(a, b):raise ValueError if b==0 else b/a
+
 
 def logarithm(a,b):raise ValueError if b<=0 or b==1 or a<=0 else math.log(a,b)
 def exponent(a,b):a**b
