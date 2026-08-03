31fc77bea79664d6b7578656b433833d5132686a
kyleziegler-123 <kyleziegler@ufl.edu>
2026-08-03 18:56:06 -0400

    stuff

--- calculator.py
+++ calculator.py
@@ -1,3 +1,7 @@
+#https://github.com/ThunderIconoclast24/lab11-KC-JC.git
+#Partner 1: John-Claude Hutchinson
+#Partner 2: Kyle Ziegler
+
 import math
 """
 calculator.py
@@ -25,13 +29,16 @@ def mul(a:float, b:float):
     return a * b
 def div(a:float, b:float):
     try:
-
-        raise ZeroDivisionError if a == 0 else b / a
+        if a == 0:
+            raise ZeroDivisionError
+        return a/b
     except ZeroDivisionError:
         print("Error")
 def log(a:float, b:float):
     try:
-        raise ValueError if b<=0 or b==1 or a<=0 else math.log(a,b)
+        if b <= 0 or a <= 0 or b==1:
+            raise ValueError
+        return math.log(a,b)
     except ValueError:
         print("Error")
 def exp(a:float, b:float):
@@ -44,10 +51,10 @@ def exp(a:float, b:float):
 
 
 # First example
-def add(a, b): a+b
-def subtract(a, b): a-b
-def multiply(a, b): a*b
+#def add(a, b): a+b
+#def subtract(a, b): a-b
+#def multiply(a, b): a*b
 
 
-def logarithm(a,b):raise ValueError if b<=0 or b==1 or a<=0 else math.log(a,b)
-def exponent(a,b):a**b
+#def logarithm(a,b):raise ValueError if b<=0 or b==1 or a<=0 else math.log(a,b)
+#def exponent(a,b):a**b
